285d1fec5524ca55634fa58ed6a36aed09164fd9
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-03-23 17:21:25 -0400

    Challenge 4.3, Release 2: Change around code so proper data conversion

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,7 +4,12 @@ name = gets.chomp
 
 puts "What's your hamster's volume level (1-10)?"
 
-volume = gets.chomp
+loop do
+  volume = gets.chomp
+  volume = volume.to_i
+  break if volume >= 1 && volume <= 10
+  puts "Please enter a number between 1 and 10."
+end
 
 puts "What's your hamster's fur color?"
 
@@ -14,10 +19,20 @@ puts "Do you hamster think your hamster is a good candidate for adoption (y/n)?"
 
 adoptable = gets.chomp
 
+if adoptable == "y"
+  adoptable = true
+elsif adoptable == "n"
+  adoptable =  false
+else
+  puts "Sorry, invalid input."
+end
+
 puts "What's your hamster's estimated age?"
 
 age = gets.chomp
 
 if age == ""
   age = nil
+else
+  age = age.to_i
 end
