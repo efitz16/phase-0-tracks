1a1c499c65af1f285465b722e2b740144374f5dd
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-03-23 21:33:31 -0400

    Challenge 4.4, Release 1: write code to gather employee info

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "What year were you born?"
+
+date_of_birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance (y/n)?"
+
+insurance = gets.chomp
