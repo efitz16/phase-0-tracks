74ec8250d507d7eea615cbe302225c6416ba2528
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-03-25 18:39:56 -0400

    Challenge 4.6: Release 2 part II, wrote decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,6 +1,22 @@
 def encrypt(x)
-	x.next
-	x.delete([0])
+  x = x + x[-1].next
+  x.delete(x[0])
 end
 
 encrypt("abc")
+
+def decrypt
+  answer = encrypt("abc")
+  a = "abcdefghijklmnopqrstuvwxyz".index(answer[0])
+  
+  answer.reverse!
+  
+  answer = answer + "abcdefghijklmnopqrstuvwxyz"[a-1]
+
+  answer = answer.delete(answer[0])
+  
+p    answer.reverse!
+  
+end
+
+decrypt
