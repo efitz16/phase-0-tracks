d63f9513821fa9cef80a09b416d2a175fb24ab26
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-03-25 20:36:01 -0400

    CORRECT Challenge 4.6, Release 2, part I: encrypt method--rewrite LOOPING version that increases each individual letter

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,22 +1,23 @@
-def encrypt(x)
-  x = x + x[-1].next
-  x.delete(x[0])
+def encrypt(a)
+  
+  len = a.length
+  
+  index = 0
+
+  while index < len
+    a = a + a[index].next
+    index += 1
+  end
+  
+  i = len - 1
+  
+  while i >= 0
+    a[i] = ""
+    i -= 1
+  end
+  
+p a
+
 end
 
 encrypt("abc")
-
-def decrypt
-  answer = encrypt("abc")
-  a = "abcdefghijklmnopqrstuvwxyz".index(answer[0])
-  
-  answer.reverse!
-  
-  answer = answer + "abcdefghijklmnopqrstuvwxyz"[a-1]
-
-  answer = answer.delete(answer[0])
-  
-p    answer.reverse!
-  
-end
-
-decrypt
