19c11d62d7a92d91ca2fe6f17d0f5e0cc67097d1
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-03-29 17:37:37 -0400

    Challenge 5.1: complete assignment, find all methods that create given strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -10,30 +10,31 @@
 “zom”.insert(2, "o")
 # => “zoom”
 
-# “enhance”.<???>
+"enhance".center(15)
 #=> “    enhance    ”
+#=> "    enhance    "
 
 “Stop! You’re under arrest!”.upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-#“the usual”.<???>
+“the usual”.concat(" suspects")
 #=> “the usual suspects”
 
-#“ suspects”.<???>
+“ suspects”.insert(0, "the usual")
 #=> “the usual suspects”
 
 “The case of the disappearing last letter”.chop
 # => “The case of the disappearing last lette”
 
-#“The mystery of the missing first letter”.<???>
+“The mystery of the missing first letter”.reverse.chop.reverse
 # => “he mystery of the missing first letter”
 
-#“Elementary  ,    my   dear      Watson!”.squeeze("  ")
+"Elementary  ,    my   dear      Watson!".delete(" ").insert(11, " ").insert(14, " ").insert(19, " ")
 # => “Elementary, my dear Watson!”
 
 “z”.ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-“How many times does the letter ‘a’ appear in this string?”.count "a"
+“How many times does the letter 'a' appear in this string?”.count "a"
 #=> 4
