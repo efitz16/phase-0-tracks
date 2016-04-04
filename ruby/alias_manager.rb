94f874f1a86ec938f6e055d7fc98d5cb33b66eca
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-04 03:53:50 -0400

    Challenge 5.6, Release 1: Add user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,47 +1,59 @@
 # pseudocode
 # 
+puts "Would you like to play?"
+puts "Type 'quit' to say no or end the program."
+ans = gets.chomp
 
-puts "Give me a real name:"
+while ans != "quit"
+	puts "Give me a full name:"
 
-name = gets.chomp
-
-name = name.split(' ').reverse
-
-p name
-
-z = 0
-
-while z < name.length
-	name[z] = name[z].split(' ')
-	z += 1
-end
-
-p name
-
-p name = name.join(' ')
-
-count = 0
-
-spy_name = ""
-
-j = name.index(" ")
-
-name.downcase!
-
-while count < name.length
-	if name[count] == "a" || name[count] == "e" || name[count] == "i" || name[count] == "o" || name[count] == "u"
-		i = "aeiou".index(name[count])
-		spy_name[count] = "aeiou"[i+1]
-	elsif name[count] == " "
-		spy_name[count] = " "
+	name = gets.chomp
+	if name == "quit"
+		abort("Thanks for playing!")
 	else
-		k = "bcdfghjklmnpqrstvwxyz".index(name[count])
-		spy_name[count] = "bcdfghjklmnpqrstvwxyzb"[k+1]
+		name = name.split(' ').reverse
+		
+		#p name
+		
+		z = 0
+		
+		while z < name.length
+			name[z] = name[z].split(' ')
+			z += 1
+		end
+		
+		#p name
+		
+		p name = name.join(' ')
+		
+		#p name
+		
+		count = 0
+		
+		spy_name = ""
+		
+		j = name.index(" ")
+		
+		name.downcase!
+		
+		while count < name.length
+			if name[count] == "a" || name[count] == "e" || name[count] == "i" || name[count] 	== 	"o" || name[count] == "u"
+				i = "aeiou".index(name[count])
+				spy_name[count] = "aeiou"[i+1]
+			elsif name[count] == " "
+				spy_name[count] = " "
+			else
+				k = "bcdfghjklmnpqrstvwxyz".index(name[count])
+				spy_name[count] = "bcdfghjklmnpqrstvwxyzb"[k+1]
+			end
+			count += 1
+		end
+		
+		spy_name.capitalize!
+		spy_name[j+1] = spy_name[j+1].upcase
+		
+		puts "Your spy name is:"
+		p spy_name
 	end
-	count += 1
 end
-
-spy_name.capitalize!
-spy_name[j+1] = spy_name[j+1].upcase!
-
-p spy_name
+exit
