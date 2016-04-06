4aa3afcc6a5a48465047216623e695d16f3993c1
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-05 21:58:00 -0400

    Challenge 6.1, Release 0: in comments design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+=begin
+	
+CLASS~~~~~~~~~~~~~~~~~
+Puppy
+	
+CHARACTERISTICS~~~~~~~
+Number of legs: 4
+Number of eyes: 2
+Size range: Small
+Age range: at most 15 months
+Fur Color: varies
+Friendly: yes
+
+BEHAVIOR~~~~~~~~~~~~~~
+Bark
+Run
+Play
+Nap
+Fetch
+Eat
+
+=end
