f4f36324bca507d93a7cdd9cf72fe80d7ccdf0a7
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-07 19:25:42 -0400

    Challenge 6.2, Release 0: create instance of Puppy class, instance methods, and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,37 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(int)
+    int.times do
+      puts "Woof!"
+  end
+  end
+    
+  def roll_over
+    puts "*rolls over*"
+  end
+    
+  def dog_years(age)
+    dog_age = age*7
+    puts "Age: #{dog_age}"
+    dog_age
+  end
+  
+  def sit
+    puts "*sits*"
+  end
 
 end
 
+Rover = Puppy.new
+
+Rover.fetch("ball")
+
+Rover.speak(2)
+
+Rover.roll_over
+  
+Rover.dog_years(5)
+
+Rover.sit
