548b7d5931e73224c0642e9db331a9c6fc0fe848
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-07 21:42:46 -0400

    Challenge 6.3, Release 1, part I: update Santa class with attributes, change given driver code, and use given tests

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -12,8 +16,33 @@ class Santa
 	end
 end
 
-Harry = Santa.new
+# now that initialize takes two arguments, need to update driver code
+Harry = Santa.new("male", "black")
 
-Sam = Santa.new
+Sam = Santa.new("none", "Native American")
 
-Irene = Santa.new
+Irene = Santa.new("female", "asian")
+
+=begin
+# Example Release 1 gives:
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas #debugging
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+=end
+
