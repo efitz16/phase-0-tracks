4381a2b3b3eb6ee6b5a0ccc58837608775faa960
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-17 16:46:40 -0400

    Challenge 7.2, Release 2: Used constructor to create many instances of car class; called their functions.

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,38 @@ if (names.length == colors.length) {
 }
 
 console.log(horses);
+
+function Car(name, isFast, color, horsepower) {
+  console.log("Your new car", this);
+  
+  this.name = name;
+  this.isFast = isFast;
+  this.color = color;
+  this.horsepower = horsepower;
+  
+  this.start = function() { console.log("Rev!!!"); };
+  
+  console.log("CAR INITIALIZATION COMPLETE");
+
+}
+
+console.log("Let's build a car ...");
+var aNewCar = new Car("Ford", true, "red", 240);
+console.log(aNewCar);
+console.log("Starting the car...");
+aNewCar.start();
+console.log("----")
+
+console.log("Let's build a car ...");
+var anotherNewCar = new Car("Honda", false, "white", 180);
+console.log(anotherNewCar);
+console.log("Starting the car...");
+anotherNewCar.start();
+console.log("----")
+
+console.log("Let's build a car ...");
+var yetAnotherNewCar = new Car("BMW", true, "navy", 330);
+console.log(yetAnotherNewCar);
+console.log("Starting the car...");
+yetAnotherNewCar.start();
+console.log("----")
