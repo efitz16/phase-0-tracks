12838844fcb45488704cc003abfb21d09fffa17a
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-17 16:30:39 -0400

    Challenge 7.2, Release 1: Created object that stores two arrays, one array as keys, the other as values

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,15 @@ var names = ["Bob", "Ed", "Jane", "Tom"];
 colors.push("yellow");
 
 names.push("Steve");
+
+var horses = {};
+
+if (names.length == colors.length) {
+  for(var i = 0; i <names.length; i++) {
+  	horses[names[i]] = colors[i];
+  }
+} else {
+  console.log("Sorry arrays are different lengths");
+}
+
+console.log(horses);
