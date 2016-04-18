5c97bb91df2d11b718be129c960f9214c0b4c9fd
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-18 04:46:52 -0400

    Challenge 7.3, Release 0 pseudocode: wrote pseudocode for function returns longest string in array

---
+++ js/algos.js
@@ -0,0 +1,19 @@
+// Challenge 7.3
+
+
+//Release 0:
+
+/*
+- declare some arrays with strings of words and phrases of varying lengths
+- function that finds longest phrase--takes array as argument
+	- WHILE the array length is greater than one
+		- set count variable to zero
+		- IF the length og the string at array at index count greater than the length of the string at the next indexin the array
+			- remove the string at count + 1, which is shorter
+		- ELSE
+			- remove the string at count because it's shorter
+		- increase count by one
+- return the array
+- driver code calling function on declared arrays
+*/
+
