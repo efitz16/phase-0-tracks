c2b569651984ce714e0530a3fac6b33ef9a45d50
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-30 14:07:00 -0400

    Challenge 9.2, Release 2: write event listener code

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,13 @@ console.log("It's running!");
 var photo = document.getElementById("orange_flowers");
 
 photo.style.border = "14px solid orange";
+
+var purpleFlowerPhoto = document.getElementById("purp_flowers");
+
+function makePicLrgr(event) {
+	event.target.height="750";
+	event.target.width="960"; // because of limits in css, photo can't be wider than 960 px
+}
+
+photo.addEventListener("click", makePicLrgr);
+purpleFlowerPhoto.addEventListener("click", makePicLrgr);
