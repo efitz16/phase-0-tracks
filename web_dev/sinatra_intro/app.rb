8047b0d3b33960ac25f488e47ea16422442a5bf3
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-04-30 15:05:11 -0400

    start on chall 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,18 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "3288 Agar Place<br>Bronx, NY 10465"
+end
+
+get '/great_job' do
+  name = params[:name]
+
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+  
+end
